fb7a6183b8be06313e14fd46b1bbc4fc6302b487
Bemer <bemer@yandex-team.ru>
2014-06-02 11:58:08 +0400

    Replace post and put methods to satisfy rest API specification

--- server/routes.js
+++ server/routes.js
@@ -14,17 +14,17 @@ exports.init = function(baseUrl) {
         { name: 'index', pattern: baseUrl + '(/)' },
         { name: 'getIssues',     data: { method: 'GET' },    pattern: url + 'issues' },
         { name: 'getIssue',      data: { method: 'GET' },    pattern: url + 'issues/<issue>' },
-        { name: 'createIssue',   data: { method: 'PUT' },    pattern: url + 'issues' },
-        { name: 'updateIssue',   data: { method: 'POST' },   pattern: url + 'issues/<issue>' },
+        { name: 'createIssue',   data: { method: 'POST' },    pattern: url + 'issues' },
+        { name: 'updateIssue',   data: { method: 'PUT' },   pattern: url + 'issues/<issue>' },
         { name: 'getComments',   data: { method: 'GET' },    pattern: url + 'issues/<issue>/comments' },
         { name: 'getComment',    data: { method: 'GET' },    pattern: url + 'issues/<issue>/comments/<comment>' },
-        { name: 'createComment', data: { method: 'PUT' },    pattern: url + 'issues/<issue>/comments' },
-        { name: 'updateComment', data: { method: 'POST' },   pattern: url + 'issues/<issue>/comments/<comment>' },
+        { name: 'createComment', data: { method: 'POST' },    pattern: url + 'issues/<issue>/comments' },
+        { name: 'updateComment', data: { method: 'PUT' },   pattern: url + 'issues/<issue>/comments/<comment>' },
         { name: 'deleteComment', data: { method: 'DELETE' }, pattern: url + 'issues/<issue>/comments/<comment>' },
         { name: 'getLabels',     data: { method: 'GET' },    pattern: url + 'labels' },
         { name: 'getLabel',      data: { method: 'GET' },    pattern: url + 'labels/<label>' },
-        { name: 'createLabel',   data: { method: 'PUT' },    pattern: url + 'labels' },
-        { name: 'updateLabel',   data: { method: 'POST' },   pattern: url + 'labels/<label>' },
+        { name: 'createLabel',   data: { method: 'POST' },    pattern: url + 'labels' },
+        { name: 'updateLabel',   data: { method: 'PUT' },   pattern: url + 'labels/<label>' },
         { name: 'deleteLabel',   data: { method: 'DELETE' }, pattern: url + 'labels/<label>' }
     ].forEach(function(item) {
         susanin.addRoute(item);
