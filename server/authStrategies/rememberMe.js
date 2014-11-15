45b33661a71ae58ff5781a713bd19a2cdc311b9d
Bemer <bemer@yandex-team.ru>
2014-11-15 22:13:14 +0400

    Remove comments. Fix some codestyle issues

--- server/authStrategies/rememberMe.js
+++ server/authStrategies/rememberMe.js
@@ -1,42 +1,42 @@
 var RememberMeStrategy = require('passport-remember-me').Strategy,
-	Strategy;
+    Strategy;
 
 module.exports = {
-	init: function (passport, options) {
-		if (typeof options == null || !options.key) {
-			return passport;
-		}
+    init: function (passport, options) {
+        if (typeof options == null || !options.key) {
+            return passport;
+        }
 
-		Strategy = new RememberMeStrategy({
-				key: '_token' //Указываем имя cookie, где хранится ваш token
-			},
-			function(token, done) {
-				// Example User should be ORM for our user
-				User
-					.findOne()
-					.where({
-						autoLoginHash: token
-					})
-					.done(function(error, user) {
-						if (error) {
-							done(error);
-						} else if (!user) {
-							done(null, false);
-						} else {
-							delete user.autoLoginHash;
-							user.save(function() {});
-							done(null, user);
-						}
-					});
-			}, function(user, done) {
-				var token = crypto.randomBytes(32).toString('hex');
-				user.autoLoginHash = token;
-				user.save(function() {});
-				done(null, token);
-			})
+        Strategy = new RememberMeStrategy({
+                key: '_token' //Указываем имя cookie, где хранится ваш token
+            },
+            function(token, done) {
+                // Example User should be ORM for our user
+                User
+                    .findOne()
+                    .where({
+                        autoLoginHash: token
+                    })
+                    .done(function(error, user) {
+                        if (error) {
+                            done(error);
+                        } else if (!user) {
+                            done(null, false);
+                        } else {
+                            delete user.autoLoginHash;
+                            user.save(function() {});
+                            done(null, user);
+                        }
+                    });
+            }, function(user, done) {
+                var token = crypto.randomBytes(32).toString('hex');
+                user.autoLoginHash = token;
+                user.save(function() {});
+                done(null, token);
+            })
 
-		passport.use(Strategy);
+        passport.use(Strategy);
 
-		return passport
-	}
+        return passport;
+    }
 };
