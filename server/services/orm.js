bbf33fd5e878e98df6a8c7b81230edb0a2bd1162
Bemer <bemer@yandex-team.ru>
2014-11-15 15:42:35 +0400

    Implement orm inheritance

--- server/services/orm.js
+++ server/services/orm.js
@@ -1,56 +1,56 @@
-var ORM = function(options) {
-    this.init(options);
+var Base = require('./base'),
+    ORM = function(options) {
+        this.init(options);
+    };
+
+ORM.prototype = Object.create(Base.prototype);
+
+ORM.prototype.init = function(options) {
+    //TODO It should be implemented
 };
 
-ORM.prototype = {
+ORM.prototype.getIssues = function(options) {
+    //TODO It should be implemented
+};
 
-    init: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.getIssue = function(options) {
+    //TODO It should be implemented
+};
 
-    getIssues: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.createIssue = function(options) {
+    //TODO It should be implemented
+};
 
-    getIssue: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.editIssue = function(options) {
+    //TODO It should be implemented
+};
 
-    createIssue: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.getComments = function(options) {
+    //TODO It should be implemented
+};
 
-    editIssue: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.createComment = function(options) {
+    //TODO It should be implemented
+};
 
-    getComments: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.editComment = function(options) {
+    //TODO It should be implemented
+};
 
-    createComment: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.deleteComment = function(options) {
+    //TODO It should be implemented
+};
 
-    editComment: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.getLabels = function(options) {
+    //TODO It should be implemented
+};
 
-    deleteComment: function(options) {
-        //TODO It should be implemented
-    },
+ORM.prototype.getAuthUser = function(options) {
+    //TODO It should be implemented
+};
 
-    getLabels: function(options) {
-        //TODO It should be implemented
-    },
-
-    getAuthUser: function(options) {
-        //TODO It should be implemented
-    },
-
-    getRepoInfo: function(options) {
-        //TODO It should be implemented
-    }
+ORM.prototype.getRepoInfo = function(options) {
+    //TODO It should be implemented
 };
 
 module.exports = ORM;
