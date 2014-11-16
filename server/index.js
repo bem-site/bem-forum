923083ba3567cb64065982e4de4e107efe2635e5
amel <amel.truelife@gmail.com>
2014-11-16 11:59:10 +0300

    Setting forum cookie

--- server/index.js
+++ server/index.js
@@ -29,22 +29,25 @@ app
     .use(morgan('default')) // todo remove it after development
     .use(cookieParser()) // also is necessary for forum
     .use(bodyParser()) // also is necessary for forum
-    .use(session({ secret: 'forum-session', saveUninitialized: true, resave: true, cookie: {maxAge: 24*60*60*1000}  }))
+    .use(session({ secret: 'forum-session', saveUninitialized: true, resave: true }))
     .use(passport.initialize())
     .use(passport.session())
     .use(flash());
 
 app.get('/auth/github', passport.authenticate('github'));
 
-// handle the callback after facebook has authenticated the user
-app.get('/auth/github/callback',
-    passport.authenticate('github', {
-        successRedirect: '/',
-        failureRedirect: '/'
-    }));
+app.get('/auth/github/callback', function(req, res, next){
+    passport.authenticate('github', function (err, user, info) {
+        var expires = new Date(Date.now() + (86400000 * 5)); // 5 days
+        res.cookie('forum_username', user.username, { expires: expires });
+        // res.cookie('forum_token', access_token, { expires: expires });
+        return res.redirect('/');
+    })(req, res, next);
+});
 
 app.use(forum('/', forumOptions, passport)) // forum middleware
     .use(function(req, res) {
+        req.__data.forum = false;
         return template.run(_.extend({ block: 'page' }, req.__data), req)
             .then(function(html) {
                 res.end(html);
