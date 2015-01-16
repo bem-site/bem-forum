e6cce5874d64ba0262cf85814870ad8fce25a2ac
tavriaforever <tavria@yandex-team.ru>
2015-01-16 13:20:19 +0300

    Fix PR issues

--- desktop.blocks/comment/comment.i18n/en.js
+++ desktop.blocks/comment/comment.i18n/en.js
@@ -1,5 +1,6 @@
 module.exports = {
     "comment": {
+        "add-form-placeholder": "Leave a comment",
         "edit-form-save": "Save edit",
         "edit-form-cancel": "Cancel",
         "edit": "Edit",
