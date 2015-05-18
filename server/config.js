d8b2dd7c85cf7fdae4034f9f721018df1f2788a3
tavriaforever <tavria@yandex-team.ru>
2015-05-18 11:17:27 +0300

    Add jsdoc to config module

--- server/config.js
+++ server/config.js
@@ -1,3 +1,14 @@
+/**
+ * Module for reading and merging of configs for app forum
+ *
+ * Join the configs of the two directories:
+ * common + folder with config files relative to the current environment (development || production)
+ *
+ * 1. common/node.json - contains general not secret settings that are common to all environments
+ * 2. common/credentials.json - contains secret settings that are common to all environments
+ * 3. {ENV}/credentials.json - contains secret settings that are relevant only for the current environment
+ */
+
 var nconf = require('nconf'),
     path = require('path'),
     configsDir = path.resolve(process.cwd(), 'configs');
